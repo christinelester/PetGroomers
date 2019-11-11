1e6517d0e4889e1feb56e22d60ef154b5ca6f142
ThaddeusMorris <thad18524@sbcglobal.net>
2019-11-11 14:04:55 -0600

    Owner has a phone number

--- Owner.java
+++ Owner.java
@@ -2,15 +2,21 @@
 public class Owner {
 	
 	private String name;
+	private String phoneNum;
 	private boolean isVip;
 	
-	public Owner(String name, boolean isVip) {
+	public Owner(String name, String phoneNum, boolean isVip) {
 		this.name = name;
+		this.phoneNum = phoneNum;
 		this.isVip = isVip;
 	}
 	
 	public String getName() {
 		return name;
+	}
+	
+	public String getPhoneNum() {
+		return phoneNum;
 	}
 	
 	public boolean isVip() {
