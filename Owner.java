e0f18f0c76573697c3b03f6ccd749e7c4cea8d50
ThaddeusMorris <thad18524@sbcglobal.net>
2019-11-11 14:01:33 -0600

    Add Owner

---
+++ Owner.java
@@ -0,0 +1,20 @@
+
+public class Owner {
+	
+	private String name;
+	private boolean isVip;
+	
+	public Owner(String name, boolean isVip) {
+		this.name = name;
+		this.isVip = isVip;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public boolean isVip() {
+		return isVip;
+	}
+
+}
